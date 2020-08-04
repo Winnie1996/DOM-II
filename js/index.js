2437dc6e1ead2a1ad025dcf437bf8d22a6060c53
HungT <64987549+Winnie1996@users.noreply.github.com>
2020-08-04 15:26:19 -0700

    Finished

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // Your code goes here
+
+// #1
 const paragraphColor = document.querySelectorAll("body p")
 // console.log(paragraphColor)
 
@@ -7,12 +9,22 @@ paragraphColor.forEach(function(paragraph){
         event.target.style.color = "teal"
         //this.style.color also works'
         setTimeout(function(){
-            event.target.style.color = "black"
+            event.target.style.color = ""
         },700)
 })
 })
 
+//#2
+paragraphColor.forEach(function(paragraph){
+paragraph.addEventListener("mouseout", function(event){
+    event.target.style.color = "orange"
+    setTimeout(function(){
+        event.target.style.color = ""
+    },700)
+})
+})
 
+// #3
 const titleColor = document.querySelectorAll("body")
 // console.log(h2Color)
 
@@ -39,27 +51,71 @@ titleColor.forEach(function(headerColor){
 
 
 
-
-
-
-
-
+// #4
 const imageEnlarge = document.querySelector(".intro img:first-child")
-console.log(imageEnlarge)
+// console.log(imageEnlarge)
 let scale = 1;
 
 imageEnlarge.addEventListener("wheel", function(event){
     scale += event.deltaY * -0.01;
 
-    // Restrict scale
     scale = Math.min(Math.max(.125, scale), 4);
-  
-    // Apply scale transform
+
     imageEnlarge.style.transform = `scale(${scale})`;
 })
 
 
-    
+
+
+// #5
+window.addEventListener('load', function(event){
+    console.log('welcome to the loaded page');
+  });
 
 
 
+// #6
+const imgQuality = document.querySelector(".img-content img:first-child")
+// console.log(imgQuality)
+
+imgQuality.addEventListener('dblclick',function(event){
+    event.target.style.border = '10px solid gold'
+},true)
+
+const h1 = document.querySelector('h1')
+
+
+// #7
+h1.addEventListener('mousedown', function(event){
+    h1.textContent = 'Welcome To The Bus Enjoy The Ride'
+})
+
+
+
+//#8 9 and 10
+const header2 = document.getElementsByTagName('h2');
+console.log (header2)
+
+header2[0].addEventListener('mouseover', (event) => {
+   header2[0].style.fontSize = "8rem"
+   header2[0].style.fontWeight = '4rem'
+   header2[0].style.color = 'magenta'
+})
+
+header2[1].addEventListener('mousemove', (event) => {
+    header2[1].style.fontWeight = '8rem'
+    header2[1].style.fontSize = '10rem'
+    header2[1].style.color = 'pink'
+})
+
+header2[2].addEventListener('mousemove', (event) => {
+    header2[2].style.fontWeight = '8rem'
+    header2[2].style.fontSize = '10rem'
+    header2[2].style.color = 'purple'
+})
+
+header2[2].addEventListener('mousemove', (event) => {
+    header2[3].style.fontWeight = '8rem'
+    header2[3].style.fontSize = '10rem'
+    header2[3].style.color = 'purple'
+})
